96e349634e8023af00358bb83553045ed6de5c79
PatrickDCullen <cullenpatrickd@gmail.com>
2021-06-19 21:24:57 -0400

    End of Laracasts Learn Inertia Ep 1: Hello Ping

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,5 +7,4 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-    // I'm making a comment here to test git.
 }
